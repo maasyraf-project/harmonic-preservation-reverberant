31a77ed9c2cb8d23af04bd515dc87b001b701283
maasyraf <maasyraf.edu@gmail.com>
2023-06-01 03:02:47 +0700

    update on subband statistic analysis

--- subband_analysis.m
+++ subband_analysis.m
@@ -14,8 +14,31 @@ addpath(genpath('additional-packages'))
 
 % concatenate all loaded data
 raw_data = {an, lrev, hrev};
-max_len_data = max([length(an), length(lrev), length(hrev)]);
-t = 0:(1/fs):(max_len_data-1)/fs;
+
+% calculate start- and end-point on the audio
+idx_start = [];
+idx_end = [];
+for i = 1:length(raw_data)
+    l_start = find(raw_data{i}(:,1), 1, "first");
+    r_start = find(raw_data{i}(:,2), 1, "first");
+    
+    l_end = find(raw_data{i}(:,1), 1, "last");
+    r_end = find(raw_data{i}(:,2), 1, "last");
+    
+    idx_start = [idx_start; [l_start, r_start]]; 
+    idx_end = [idx_end; [l_end, r_end]]; 
+end
+
+idx_start = min(idx_start(:));
+idx_end = min(idx_end(:));
+for i = 1:length(raw_data)
+    l_sig = raw_data{i}(idx_start:idx_end,1);
+    r_sig = raw_data{i}(idx_start:idx_end,2);
+    raw_data{i} = [l_sig, r_sig]; 
+end
+
+len_data = length(idx_start:idx_end);
+t = 0:(1/fs):(len_data-1)/fs;
 
 %% vocoder preparation
 % parameter
@@ -27,16 +50,6 @@ par.center_frequencies_hz_auralisation = [357 548 689 968 1483 2228 3319 4670 66
 par.bandwidth_factor = [1 1 1 1 1 1 1 1 1 1 1 1].*3;
 par.weights = [0.98 0.98 0.98 0.68 0.68 0.45 0.45 0.2 0.2 0.15 0.15 0.15]';
 
-% resampling
-if par.voc_sampling_frequency_hz ~= fs
-    for i = 1:length(raw_data)
-        raw_data{i} = resample(raw_data{i}, par.voc_sampling_frequency_hz, fs);
-    end
-    fs = par.voc_sampling_frequency_hz;
-    max_len_data = max([length(raw_data{1}), length(raw_data{2}), length(raw_data{3})]);
-    t = 0:(1/fs):(max_len_data-1)/fs;
-end
-
 %% pre-emphasis filter
 pe_data = cell(1, length(raw_data));
 for i = 1:length(raw_data)
@@ -45,6 +58,16 @@ for i = 1:length(raw_data)
     l_sig = filter(b, a, raw_data{i}(:,1));
     r_sig = filter(b, a, raw_data{i}(:,2));
     pe_data{i} = [l_sig, r_sig]; 
+end
+
+%% resampling 
+if par.voc_sampling_frequency_hz ~= fs
+    for i = 1:length(pe_data)
+        pe_data{i} = resample(pe_data{i}, par.voc_sampling_frequency_hz, fs);
+    end
+    fs = par.voc_sampling_frequency_hz;
+    max_len_data = max([length(pe_data{1}), length(pe_data{2}), length(pe_data{3})]);
+    t = 0:(1/fs):(max_len_data-1)/fs;
 end
 
 %% decompose into subband
@@ -108,24 +131,28 @@ for i = 1:length(env_data)
 
 end
 
+
 %% extract linear prediction of envelope
-env_lp_lpc_data = cell(1, length(env_data));
+env_lp_lpc_data = cell(1, length(env_lp_data));
 sig_vad = cell(1, length(raw_data));
 for i = 1:length(env_data)
-    l_sub_buf = nan(size(env_data{i}{1}));
-    r_sub_buf = nan(size(env_data{i}{2}));
+    l_sub_buf = nan(size(env_lp_data{i}{1}));
+    r_sub_buf = nan(size(env_lp_data{i}{2}));
 
     % split audio into several frames
-    lenFrames = 500;
-    lenOverlap = 250;
-    nFrames = ceil(size(env_data{i}{1},2)/lenFrames);
+    lenFrames = 0.03*fs;
+    lenOverlap = 0.5*lenFrames;
+    nFrames = ceil(size(env_lp_data{i}{1},2)/lenFrames);
 
-    l_vad_buf = nan(size(env_data{i}{1},1),nFrames);
-    r_vad_buf = nan(size(env_data{i}{2},1),nFrames);
+    l_vad_buf = nan(size(env_lp_data{i}{1},1),nFrames);
+    r_vad_buf = nan(size(env_lp_data{i}{2},1),nFrames);
 
     for j = 1:size(env_data{1}{1},1)
-        l = env_data{i}{1}(j,:);
-        r = env_data{i}{2}(j,:);
+        lv = fine_data{i}{1}(j,:);
+        rv = fine_data{i}{2}(j,:);
+
+        l = env_lp_data{i}{1}(j,:);
+        r = env_lp_data{i}{2}(j,:);
 
         % conduct VAD
         VADPar = InitVADPar(fs,lenFrames,lenOverlap);
@@ -141,6 +168,22 @@ for i = 1:length(env_data)
         % y_vad = zeros(size(l));
 
         for n = 1:nFrames
+            % calculate VAD
+            lv_frame = lv(idxStart:idxEnd);
+            rv_frame = rv(idxStart:idxEnd);
+            
+            if or(length(lv_frame) < lenOverlap, length(rv_frame) < lenOverlap)
+                lv_frame = [lv_frame zeros(1, lenOverlap - length(lv_frame))];
+                rv_frame = [rv_frame zeros(1, lenOverlap - length(rv_frame))];
+            end
+           
+            [l_voiced,VADPar] = VAD(lv_frame', VADPar);
+            [r_voiced,VADPar] = VAD(rv_frame', VADPar);
+
+            l_vad = [l_vad, l_voiced];
+            r_vad = [r_vad, r_voiced];
+            
+            % conduct LP-analysis
             l_frame = l(idxStart:idxEnd);
             r_frame = r(idxStart:idxEnd);
 
@@ -148,43 +191,26 @@ for i = 1:length(env_data)
                 l_frame = [l_frame zeros(1, lenOverlap - length(l_frame))];
                 r_frame = [r_frame zeros(1, lenOverlap - length(r_frame))];
             end
-            
-            % calculate VAD
-            [lv,VADPar] = VAD(l_frame', VADPar);
-            [rv,VADPar] = VAD(r_frame', VADPar);
 
-            l_vad = [l_vad, lv];
-            r_vad = [r_vad, rv];
+            test = 1;
 
-            % y_vad(idxStart:idxEnd) = lv;
-
-            if lv == 1 
+            if test%l_voiced == 1 
                 % apply LPC on voiced segments
-                la = lpc(l_frame, 12);
+                la = lpc(l_frame, 3);
                 estl_frame = filter([0 -la(2:end)], 1, l_frame);
                 estl(idxStart:idxEnd) = estl(idxStart:idxEnd) + estl_frame;
-            else 
-                estl(idxStart:idxEnd) = estl(idxStart:idxEnd) + l(idxStart:idxEnd);
             end
 
-            if rv == 1 
+            if test%r_voiced == 1 
                 % apply LPC on voiced segments
-                ra = lpc(r_frame, 12);
+                ra = lpc(r_frame, 3);
                 estr_frame = filter([0 -ra(2:end)], 1, r_frame);
                 estr(idxStart:idxEnd) = estr(idxStart:idxEnd) + estr_frame;
-
-            else
-                estr(idxStart:idxEnd) = estr(idxStart:idxEnd) + r(idxStart:idxEnd);
             end
             
             % update index for selecting next frame
             idxStart = idxStart + lenOverlap;
             idxEnd = idxEnd + lenOverlap;
-
-            % check whether NaN data appear during processing
-            if anynan(estr) == 1
-                warning(strcat("NaN value appear ", string(n)))
-            end
         
         end
 
@@ -195,24 +221,29 @@ for i = 1:length(env_data)
         l_sig = l - estl;
         r_sig = r - estr;
 
+        % check whether NaN data appear during processing
+        if anynan(l_sig) == 1
+            warning(strcat("NaN value appear ", string(j)))
+        end
+
         l_sub_buf(j,:) = l_sig;
         r_sub_buf(j,:) = r_sig;
 
     end
     
-    sig_vad = {l_vad_buf, r_vad_buf};
+    sig_vad{i} = {l_vad_buf, r_vad_buf};
     env_lp_lpc_data{i} = {l_sub_buf, r_sub_buf};
 
 end
 
-%% extract kurtosis data of LP-residual
-env_lp_kurt_data = cell(1, length(env_lp_data));
-for i = 1:length(env_lp_data)
+%% extract kurtosis data of LP-residual of envelope
+env_lp_kurt_data = cell(1, length(env_lp_lpc_data));
+for i = 1:length(env_lp_lpc_data)
     l_sub_buf = nan(size(env_lp_data{i}{1}));
     r_sub_buf = nan(size(env_lp_data{i}{2}));
 
     for j = 1:size(env_data{1}{1},1)
-        lseg = 400;
+        lseg = 0.03*fs;
         theta = 1e-6;
         l_sig = kurt(env_lp_lpc_data{i}{1}(j,:), lseg, theta);
         r_sig = kurt(env_lp_lpc_data{i}{2}(j,:), lseg, theta);
@@ -224,7 +255,113 @@ for i = 1:length(env_lp_data)
 
 end
 
-% conduct adaptive filtering
+%%
+env_lp_lpc_kurt_data = cell(1, length(env_data));
+sig_vad = cell(1, length(raw_data));
+for i = 1:length(env_data)
+    l_sub_buf = nan(size(env_lp_lpc_data{i}{1}));
+    r_sub_buf = nan(size(env_lp_lpc_data{i}{2}));
+
+    % split audio into several frames
+    lenFrames = 0.03*fs;
+    lenOverlap = 0.5*lenFrames;
+    nFrames = ceil(size(env_lp_lpc_data{i}{1},2)/lenFrames);
+
+    l_vad_buf = nan(size(env_lp_lpc_data{i}{1},1),nFrames);
+    r_vad_buf = nan(size(env_lp_lpc_data{i}{2},1),nFrames);
+
+    for j = 1:size(env_lp_lpc_data{1}{1},1)
+        lv = env_lp_lpc_data{i}{1}(j,:);
+        rv = env_lp_lpc_data{i}{2}(j,:);
+
+        l = env_lp_lpc_data{i}{1}(j,:);
+        r = env_lp_lpc_data{i}{2}(j,:);
+
+        % conduct VAD
+        VADPar = InitVADPar(fs,lenFrames,lenOverlap);
+        idxStart = 1;
+        idxEnd = lenOverlap;
+
+        l_vad = [];
+        r_vad = [];
+
+        estl = zeros(size(l));
+        estr = zeros(size(r));
+
+        % y_vad = zeros(size(l));
+
+        for n = 1:nFrames
+            % calculate VAD
+            lv_frame = lv(idxStart:idxEnd);
+            rv_frame = rv(idxStart:idxEnd);
+            
+            if or(length(lv_frame) < lenOverlap, length(rv_frame) < lenOverlap)
+                lv_frame = [lv_frame zeros(1, lenOverlap - length(lv_frame))];
+                rv_frame = [rv_frame zeros(1, lenOverlap - length(rv_frame))];
+            end
+           
+            [l_voiced,VADPar] = VAD(lv_frame', VADPar);
+            [r_voiced,VADPar] = VAD(rv_frame', VADPar);
+
+            l_vad = [l_vad, l_voiced];
+            r_vad = [r_vad, r_voiced];
+            
+            % extract kurtosis
+            l_frame = l(idxStart:idxEnd);
+            r_frame = r(idxStart:idxEnd);
+
+            if or(length(l_frame) < lenOverlap, length(r_frame) < lenOverlap)
+                l_frame = [l_frame zeros(1, lenOverlap - length(l_frame))];
+                r_frame = [r_frame zeros(1, lenOverlap - length(r_frame))];
+            end
+
+            if l_voiced == 1 
+                % extract kurtosis on voiced segments
+                estl_frame = kurtosis(l_frame);
+                estl_frame(isnan(estl_frame)) = 0;
+                estl(idxStart:idxEnd) = estl_frame;
+            else 
+                estl(idxStart:idxEnd) = estl(idxStart:idxEnd) + l(idxStart:idxEnd);
+            end
+
+            if r_voiced == 1 
+                % extract kurtosis on voiced segments
+                estr_frame = kurtosis(r_frame);
+                estr_frame(isnan(estr_frame)) = 0;
+                estr(idxStart:idxEnd) = estr_frame;
+            else
+                estr(idxStart:idxEnd) = estr(idxStart:idxEnd) + r(idxStart:idxEnd);
+            end
+            
+            % update index for selecting next frame
+            idxStart = idxStart + lenOverlap;
+            idxEnd = idxEnd + lenOverlap;
+        
+        end
+
+        l_vad_buf(j,:) = l_vad;
+        r_vad_buf(j,:) = r_vad;
+        
+        % calculating LP residual signal
+        l_sig = l - estl;
+        r_sig = r - estr;
+
+        % check whether NaN data appear during processing
+        if anynan(l_sig) == 1
+            warning(strcat("NaN value appear ", string(j)))
+        end
+
+        l_sub_buf(j,:) = l_sig;
+        r_sub_buf(j,:) = r_sig;
+
+    end
+    
+    sig_vad = {l_vad_buf, r_vad_buf};
+    env_lp_lpc_kurt_data{i} = {l_sub_buf, r_sub_buf};
+
+end
+
+%% conduct adaptive filtering on envelope
 env_filtered_data = cell(1, length(env_lp_data));
 rms_env_filtered_data = cell(1, length(env_lp_data));
 for i = 2:length(env_lp_kurt_data)
@@ -512,15 +649,56 @@ data_label = ["clean speech", "low reverberant speech"];
 %     end
 % end
 
-%% plot envelope of data
+%% plot fine structure of data
+data_label = ["clean speech", "low reverberant speech"];
+%data_label = ["clean speech", "high reverberant speech"];
 figure
-for i = 1:2:3%length(env_lp_data)
+for i = 1:2%linspace(2,1,2)
     for j = 1:6%size(env_data{1}{1},1)
         subplot(6,1,j)
         if i == 1
-            plot(t(1:length(env_lp_data{i}{2}(j,:))), env_lp_data{i}{2}(j,:), "Color", [0 0 0 1])
+            plot(t(1:length(fine_data{i}{1}(j,:))), fine_data{i}{1}(j,:), "Color", [0 0 0 1])
         else
-            plot(t(1:length(env_lp_data{i}{2}(j,:))), env_lp_data{i}{2}(j,:), "Color", [0.5 0.5 0.5 0.8])
+            plot(t(1:length(fine_data{i}{1}(j,:))), fine_data{i}{1}(j,:), "Color", [0.5 0.5 0.5 0.8])
+        end
+        legend(data_label)
+        hold on
+        ylabel("Subband Amplitude")
+        if j == 6
+            xlabel("Time (s)")
+        end
+    end
+end
+
+figure
+for i = 1:2%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(fine_data{i}{1}(j+6,:))), fine_data{i}{1}(j+6,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(fine_data{i}{1}(j+6,:))), fine_data{i}{1}(j+6,:), "Color", [0.5 0.5 0.5 0.8])
+        end
+        legend(data_label)
+        hold on
+        ylabel("Subband Amplitude")
+        if j == 6
+            xlabel("Time (s)")
+        end
+    end
+end
+
+%% plot envelope (no LP filtered) of data
+data_label = ["clean speech", "low reverberant speech"];
+%data_label = ["clean speech", "high reverberant speech"];
+figure
+for i = 1:2%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(env_data{i}{1}(j,:))), env_data{i}{1}(j,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(env_data{i}{1}(j,:))), env_data{i}{1}(j,:), "Color", [0.5 0.5 0.5 0.8])
         end
         legend(data_label)
         hold on
@@ -532,13 +710,52 @@ for i = 1:2:3%length(env_lp_data)
 end
 
 figure
-for i = 1:2:3%length(env_lp_data)
+for i = 1:2%length(env_lp_data)
     for j = 1:6%size(env_data{1}{1},1)
         subplot(6,1,j)
         if i == 1
-            plot(t(1:length(env_lp_data{i}{2}(j+6,:))), env_lp_data{i}{2}(j+6,:), "Color", [0 0 0 1])
+            plot(t(1:length(env_data{i}{1}(j+6,:))), env_data{i}{1}(j+6,:), "Color", [0 0 0 1])
         else
-            plot(t(1:length(env_lp_data{i}{2}(j+6,:))), env_lp_data{i}{2}(j+6,:), "Color", [0.5 0.5 0.5 0.8])
+            plot(t(1:length(env_data{i}{1}(j+6,:))), env_data{i}{1}(j+6,:), "Color", [0.5 0.5 0.5 0.8])
+        end
+        legend(data_label)
+        hold on
+        ylabel("Envelope Amplitude")
+        if j == 6
+            xlabel("Sample")
+        end
+    end
+end
+
+%% plot envelope of data
+data_label = ["clean speech", "low reverberant speech"];
+%data_label = ["clean speech", "high reverberant speech"];
+figure
+for i = 1:2%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(env_lp_data{i}{1}(j,:))), env_lp_data{i}{1}(j,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(env_lp_data{i}{1}(j,:))), env_lp_data{i}{1}(j,:), "Color", [0.5 0.5 0.5 0.8])
+        end
+        legend(data_label)
+        hold on
+        ylabel("Envelope Amplitude")
+        if j == 6
+            xlabel("Sample")
+        end
+    end
+end
+
+figure
+for i = 1:2%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(env_lp_data{i}{1}(j+6,:))), env_lp_data{i}{1}(j+6,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(env_lp_data{i}{1}(j+6,:))), env_lp_data{i}{1}(j+6,:), "Color", [0.5 0.5 0.5 0.8])
         end
         legend(data_label)
         hold on
@@ -550,34 +767,126 @@ for i = 1:2:3%length(env_lp_data)
 end
 
 %% plot LP-analysis results
+%data_label = ["clean speech", "low reverberant speech"];
+data_label = ["clean speech", "high reverberant speech"];
 figure
-for i = 1:length(env_lp_lpc_data)
-    for j = 1:4%size(env_data{1}{1},1)
-        subplot(4,1,j)
-        plot(env_lp_lpc_data{i}{2}(j,:))
+for i = 1:2:3%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(env_lp_lpc_data{i}{1}(j,:))), env_lp_lpc_data{i}{1}(j,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(env_lp_lpc_data{i}{1}(j,:))), env_lp_lpc_data{i}{1}(j,:), "Color", [0.5 0.5 0.5 0.8])
+        end
         legend(data_label)
         hold on
-        ylabel("LPC Amplitude")
-        if j == 4
+        ylabel("Envelope Amplitude")
+        if j == 6
+            xlabel("Sample")
+        end
+    end
+end
+
+figure
+for i = 1:2:3%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(env_lp_lpc_data{i}{1}(j+6,:))), env_lp_lpc_data{i}{1}(j+6,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(env_lp_lpc_data{i}{1}(j+6,:))), env_lp_lpc_data{i}{1}(j+6,:), "Color", [0.5 0.5 0.5 0.8])
+        end
+        legend(data_label)
+        hold on
+        ylabel("Envelope Amplitude")
+        if j == 6
+            xlabel("Sample")
+        end
+    end
+end
+
+%% plot kurtosis of LP-residual of envelope
+%data_label = ["clean speech", "low reverberant speech"];
+data_label = ["clean speech", "high reverberant speech"];
+figure
+for i = 1:2:3%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(env_lp_kurt_data{i}{1}(j,:))), env_lp_kurt_data{i}{1}(j,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(env_lp_kurt_data{i}{1}(j,:))), env_lp_kurt_data{i}{1}(j,:), "Color", [0.5 0.5 0.5 0.8])
+        end
+        legend(data_label)
+        hold on
+        ylabel("Envelope Amplitude")
+        if j == 6
+            xlabel("Sample")
+        end
+    end
+end
+
+figure
+for i = 1:2:3%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(env_lp_kurt_data{i}{1}(j+6,:))), env_lp_kurt_data{i}{1}(j+6,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(env_lp_kurt_data{i}{1}(j+6,:))), env_lp_kurt_data{i}{1}(j+6,:), "Color", [0.5 0.5 0.5 0.8])
+        end
+        legend(data_label)
+        hold on
+        ylabel("Envelope Amplitude")
+        if j == 6
             xlabel("Sample")
         end
     end
 end
 
 %%
+data_label = ["clean speech", "low reverberant speech"];
+%data_label = ["clean speech", "high reverberant speech"];
+
+chan = 1;
+
 figure
-for i = 1:length(env_lp_kurt_data)
-    for j = 1:4%size(env_data{1}{1},1)
-        subplot(4,1,j)
-        plot(env_lp_kurt_data{i}{1}(j,:))
+for i = 1%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(env_lp_lpc_kurt_data{i}{chan}(j,:))), env_lp_lpc_kurt_data{i}{chan}(j,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(env_lp_lpc_kurt_data{i}{chan}(j,:))), env_lp_lpc_kurt_data{i}{chan}(j,:), "Color", [0.5 0.5 0.5 0.8])
+        end
         legend(data_label)
         hold on
-        ylabel("Kurtosis Amplitude")
-        if j == 4
+        ylabel("Envelope Amplitude")
+        if j == 6
             xlabel("Sample")
         end
     end
 end
+
+figure
+for i = 1%length(env_lp_data)
+    for j = 1:6%size(env_data{1}{1},1)
+        subplot(6,1,j)
+        if i == 1
+            plot(t(1:length(env_lp_lpc_kurt_data{i}{chan}(j+6,:))), env_lp_lpc_kurt_data{i}{chan}(j+6,:), "Color", [0 0 0 1])
+        else
+            plot(t(1:length(env_lp_lpc_kurt_data{i}{chan}(j+6,:))), env_lp_lpc_kurt_data{i}{chan}(j+6,:), "Color", [0.5 0.5 0.5 0.8])
+        end
+        legend(data_label)
+        hold on
+        ylabel("Envelope Amplitude")
+        if j == 6
+            xlabel("Sample")
+        end
+    end
+end
+
+%%
 
 figure
 for i = 1:length(env_filtered_data)
---
+++ vad_method.m
@@ -0,0 +1,37 @@
+clc
+clear all
+close all
+
+%% load all necessary packages and files
+addpath(genpath('additional-packages'))
+
+% load anechoic signal
+[an, fs] = audioread("sample_anechoic_min90.wav");
+
+% load low and high reverberant signal
+[lrev, fs] = audioread("sample_rev_low_min90.wav");
+[hrev, fs] = audioread("sample_rev_high_min90.wav");
+
+raw_data = {an, lrev, hrev};
+
+% calculate start- and end-point on the audio
+idx_start = [];
+idx_end = [];
+for i = 1:length(raw_data)
+    l_start = find(raw_data{i}(:,1), 1, "first");
+    r_start = find(raw_data{i}(:,2), 1, "first");
+    
+    l_end = find(raw_data{i}(:,1), 1, "last");
+    r_end = find(raw_data{i}(:,2), 1, "last");
+    
+    idx_start = [idx_start; [l_start, r_start]]; 
+    idx_end = [idx_end; [l_end, r_end]]; 
+end
+
+idx_start = min(idx_start(:));
+idx_end = min(idx_end(:));
+for i = 1:length(raw_data)
+    l_sig = raw_data{i}(idx_start:idx_end,1);
+    r_sig = raw_data{i}(idx_start:idx_end,2);
+    raw_data{i} = [l_sig, r_sig]; 
+end
